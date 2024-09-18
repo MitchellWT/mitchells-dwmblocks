0a3cba27810c28169ab768209edb4288280c0ed1
mitchell <mitchell444999@gmail.com>
2024-09-18 21:34:34 +1000

    Update crpyto with cool coins and fix CPU

--- blocks.def/cputemp.sh
+++ blocks.def/cputemp.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 temperature=$(sensors -u | 
-    grep -A 6 'coretemp' | 
+    grep -A 1 'Tctl' | 
     grep 'temp1_input' | 
-    grep -o '[0-9]*\.[0.9]')
+    grep -o '[0-9]*\.[0-9]')
 
 echo 🌡 $temperature°C
--- blocks.def/crypto.sh
+++ blocks.def/crypto.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
 
-adaPrice=$(curl -X 'GET' 'https://api.coingecko.com/api/v3/simple/price?ids=binance-peg-cardano&vs_currencies=aud' -H 'accept: application/json' | grep -o '[0-9]*\.[0-9]*')
-#xrpPrice=$(curl -X 'GET' 'https://api.coingecko.com/api/v3/simple/price?ids=binance-peg-xrp&vs_currencies=aud' -H 'accept: application/json' | grep -o '[0-9]*\.[0-9]*')
-bnbPrice=$(curl -X 'GET' 'https://api.coingecko.com/api/v3/simple/price?ids=binancecoin&vs_currencies=aud' -H 'accept: application/json' | grep -o '[0-9]*\.[0-9]*')
-icpPrice=$(curl -X 'GET' 'https://api.coingecko.com/api/v3/simple/price?ids=internet-computer&vs_currencies=aud' -H 'accept: application/json' | grep -o '[0-9]*\.[0-9]*')
 
-echo '🗠 ADA➔$'$adaPrice' BNB➔$'$bnbPrice' ICP➔$'$icpPrice
+btcPrice=$(curl -X 'GET' 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=aud' -H 'accept: application/json' | jq .bitcoin.aud)
+adaPrice=$(curl -X 'GET' 'https://api.coingecko.com/api/v3/simple/price?ids=binance-peg-cardano&vs_currencies=aud' -H 'accept: application/json' | grep -o '[0-9]*\.[0-9]*')
+# xrpPrice=$(curl -X 'GET' 'https://api.coingecko.com/api/v3/simple/price?ids=binance-peg-xrp&vs_currencies=aud' -H 'accept: application/json' | grep -o '[0-9]*\.[0-9]*')
+bnbPrice=$(curl -X 'GET' 'https://api.coingecko.com/api/v3/simple/price?ids=binancecoin&vs_currencies=aud' -H 'accept: application/json' | grep -o '[0-9]*\.[0-9]*')
+# icpPrice=$(curl -X 'GET' 'https://api.coingecko.com/api/v3/simple/price?ids=internet-computer&vs_currencies=aud' -H 'accept: application/json' | grep -o '[0-9]*\.[0-9]*')
+
+echo '🗠 BTC➔$'$btcPrice' ADA➔$'$adaPrice' BNB➔$'$bnbPrice''
